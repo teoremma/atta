c3285bb892fe158217a8ce297dfe31860ce92fac
emmanuel <fanayagonzalez@ucsd.edu>
2026-01-23 15:38:59 -0800

    Add params and full completions

--- attn_test.py
+++ attn_test.py
@@ -27,11 +27,9 @@ class AttentionTest:
         last_n_tokens = prefix[-n:]
         return "".join(self.tokenizer.batch_decode(last_n_tokens)).replace("\n", "\\n")
 
-    def get_hidden_states(self, prompt: str, n_completions: int):
+    def get_hidden_states(self, prompt: str, n_completions: int, max_new_tokens: int = 32) -> dict:
         inputs = self.tokenizer(prompt, return_tensors="pt").to(self.device)
         prompt_length = inputs.input_ids.shape[1]
-
-        max_new_tokens = 16
 
         generation_config = GenerationConfig(
             max_new_tokens=max_new_tokens,
@@ -182,13 +180,13 @@ class AttentionTest:
                 f.write("\n\n")
         
 
-    def plot_hidden_states(self, text_file: str, n_completions: int, metric: str = 'cosine'):
+    def plot_hidden_states(self, text_file: str, n_completions: int, metric: str = 'cosine', max_new_tokens: int = 32):
         with open(text_file, "r") as f:
             prompt = f.read()
         
         prompt_name = text_file.split("/")[-1].split(".")[0]
 
-        hidden_states_dict = self.get_hidden_states(prompt, n_completions)
+        hidden_states_dict = self.get_hidden_states(prompt, n_completions, max_new_tokens=max_new_tokens)
         prompt_ids = hidden_states_dict["prompt_ids"]
         print(self.tokenizer.batch_decode(prompt_ids[0]))
         completion_ids = hidden_states_dict["completions_ids"]
@@ -207,7 +205,20 @@ class AttentionTest:
         plot_dir = f"plots/{tms}_{prompt_name}"
         os.makedirs(plot_dir, exist_ok=True)
 
-        for layer_idx in range(num_layers):
+        # save completions
+        completions_dir = f"{plot_dir}/completions"
+        os.makedirs(completions_dir, exist_ok=True)
+        all_completions_content = []
+        for i in range(n_completions):
+            completion = self.tokenizer.decode(completion_ids[i])
+            with open(f"{completions_dir}/completion_{i}.txt", "w") as f:
+                f.write(completion)
+            all_completions_content.append(f"--- COMPLETION {i} ---\n{completion}\n")
+        
+        with open(f"{completions_dir}/all_completions.txt", "w") as f:
+            f.write("\n".join(all_completions_content))
+
+        for layer_idx in range(num_layers):            
             print("Processing layer", layer_idx)
             embeddings = {}
             layer_hidden_states = hidden_states_tensor[:, :, layer_idx, :]
@@ -241,33 +252,37 @@ class AttentionTest:
             self.find_nearest_neighbors(embeddings, n_neighbors=5, index=layer_idx, plot_dir=plot_dir, metric=metric)
         
 
-def test_attention(metric: str = 'cosine'):
-    # text_file = "test.txt"
-    # text_file = "prompts/dijkstra.txt"
-    # text_file = "prompts/dijkstra2.txt"
-    # text_file = "prompts/gcd.txt"
-    # text_file = "prompts/gcd2.txt"
-    # text_file = "prompts/hash.txt"
-    text_file = "prompts/duplicate.txt"
+def test_attention(model_id: str = None, text_file: str = None, n_completions: int = 10, metric: str = "cosine", max_new_tokens: int = 32):
+    # # text_file = "test.txt"
+    # # text_file = "prompts/dijkstra.txt"
+    # # text_file = "prompts/dijkstra2.txt"
+    # # text_file = "prompts/gcd.txt"
+    # # text_file = "prompts/gcd2.txt"
+    # # text_file = "prompts/hash.txt"
+    # text_file = "prompts/duplicate.txt"
 
-    # with open(text_file, "r") as f:
-    #     text = f.read()
+    # # with open(text_file, "r") as f:
+    # #     text = f.read()
 
-    # model_id = "Qwen/Qwen2.5-Coder-0.5B"
-    model_id = "Qwen/Qwen2.5-Coder-3B"
-    # model_id = "Qwen/Qwen2.5-Coder-7B"
+    # # model_id = "Qwen/Qwen2.5-Coder-0.5B"
+    # model_id = "Qwen/Qwen2.5-Coder-3B"
+    # # model_id = "Qwen/Qwen2.5-Coder-7B"
 
-    # n_completions = 14
-    # n_completions = 35
-    n_completions = 49
+    # # n_completions = 14
+    # # n_completions = 35
+    # n_completions = 49
 
     attention_test = AttentionTest(model_id)
     # attention_test.generate(text)
     # attention_test.get_hidden_states(text, n_completions=n_completions)
-    attention_test.plot_hidden_states(text_file, n_completions=n_completions, metric=metric)
+    attention_test.plot_hidden_states(text_file, n_completions=n_completions, metric=metric, max_new_tokens=max_new_tokens)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("--model_id", type=str)
+    parser.add_argument("--text_file", type=str)
+    parser.add_argument("--n_completions", type=int, default=10)
     parser.add_argument("--metric", type=str, default="cosine", choices=["cosine", "euclidean"])
+    parser.add_argument("--max_new_tokens", type=int, default=32)
     args = parser.parse_args()
-    test_attention(metric=args.metric)
+    test_attention(model_id=args.model_id, text_file=args.text_file, n_completions=args.n_completions, metric=args.metric, max_new_tokens=args.max_new_tokens)
